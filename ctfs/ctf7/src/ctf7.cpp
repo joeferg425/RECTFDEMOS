0d8ff9f6e17d7d201719dc129a866ee2d0af8915
Joe F <jokester425@gmail.com>
2021-10-12 18:12:20 -0700

    fixed g++ flag

--- ctfs/ctf7/src/ctf7.cpp
+++ ctfs/ctf7/src/ctf7.cpp
@@ -21,10 +21,6 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    unsigned char input[BUFSIZE] = {0};
-    char prompt[BUFSIZE] = {0};
-    char hash[BUFSIZE] = {0};
-    int charcount = 0;
     unsigned char hashes[ITEM_COUNT][BUFSIZE] =
         {
             {0x0A, 0x0F, 0x6B, 0x88, 0x35, 0x4D, 0xE7, 0xAF, 0xE8, 0x4B, 0x8A, 0x07, 0xDF, 0xAD, 0xCC, 0x26},
@@ -33,16 +29,22 @@ int main(int argc, char **argv)
         {
             "those aren't my waffles",
         };
+    unsigned char input[BUFSIZE] = {0};
+    char prompt[BUFSIZE] = {0};
+    char hash[BUFSIZE] = {0};
+    int charcount = 0;
 
     // prompt for password
     sprintf(prompt, "Please enter CTF%d password: ", CTFNUM);
     get_input(argc, argv, prompt, input, BUFSIZE);
 
     md5((uint8_t *)input, charcount, (uint8_t *)hash);
-    md5((uint8_t *)flags[0], charcount, (uint8_t *)hashes[BUFSIZE-1]);
+    md5((uint8_t *)flags[0], charcount, (uint8_t *)hashes[ITEM_COUNT-1]);
 
     if (strcmp((char *)hashes[0], hash) != 0)
     {
+        char* temp = flags[0];
+        int * temp2 = (int*)temp;
         printf("Sorry '");
         printf((char*)input);
         printf("' is not the CTF%d password\n", CTFNUM);
